a98524fd84ec121c9d280adf70a3f6681a04be40
liucj <guazi@2dfire.com>
2019-10-12 13:14:47 +0800

    [add] 单例模式线程不安全demo

--- src/com/单列模式/SingletonTest.java
+++ src/com/单列模式/SingletonTest.java
@@ -1,17 +1,64 @@
 package com.单列模式;
 
 
-import java.lang.reflect.Constructor;
+import com.单列模式.懒汉式.Singleton;
 
 public class SingletonTest {
 
-    public static void main(String[] args) throws Exception {
-
-        com.单列模式.内部类.Singleton singleton = com.单列模式.内部类.Singleton.getInstance();
-        Constructor<com.单列模式.内部类.Singleton> constructor = com.单列模式.内部类.Singleton.class.getDeclaredConstructor();
-        constructor.setAccessible(true);
-        com.单列模式.内部类.Singleton newSingleton = constructor.newInstance();
-        System.out.println(singleton == newSingleton);
+    public static void main(String[] args) {
+//        test1();
+        test2();
+//        test3();
     }
 
+    /**
+     * 线程不安全,懒汉式1
+     */
+    private static void test1() {
+        for (int i = 0; i < 5; i++) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println(Singleton.getInstance1());
+                }
+            }).start();
+        }
+        for (int i = 0; i < 5; i++) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println(Singleton.getInstance1());
+                }
+            }).start();
+        }
+    }
+
+    /**
+     * 线程不安全，懒汉式3
+     */
+    private static void test2() {
+        for (int i = 0; i < 5; i++) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println(Singleton.getInstance3());
+                }
+            }).start();
+        }
+        for (int i = 0; i < 5; i++) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println(Singleton.getInstance3());
+                }
+            }).start();
+        }
+    }
+
+    /**
+     * 线程不安全，双重校验锁没有volatile，无法重现
+     */
+    private static void test3() {
+
+    }
 }
